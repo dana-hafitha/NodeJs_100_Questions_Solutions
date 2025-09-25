c28b78eabba9d118f5c45714fbccbc2621083506
Dana Hafetha <dana.hafitha@gmail.com>
2025-09-25 18:18:01 +0300

    uploading files

---
+++ Q090/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const app = express();
+const port = 4000;
+
+// Swagger definition
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'API documentation using Swagger',
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  apis: ['./routes/*.js'], // Path to your API docs
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/docs`);
+});
